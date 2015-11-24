8cf1939637dcb4083b88dd80a2e2465b8bf62b8a
Charles Jacobs <cjacobs@microsoft.com>
2015-11-23 17:14:23 -0800

    Cleaned up code. Made shake detector run in separate fiber and raise events.

--- source/main.cpp
+++ source/main.cpp
@@ -1,28 +1,7 @@
 #include "MicroBitTouchDevelop.h"
+#include "vec3.h"
 
 // Types
-template <typename T>
-struct vec3
-{
-    T x;
-    T y;
-    T z;
-
-    vec3<T>() : x(0), y(0), z(0) {}
-    vec3<T>(T x, T y, T z) : x(x), y(y), z(z) {}
-
-    template <typename S>
-    explicit vec3<T>(const vec3<S>& v)
-    {
-        x = v.x;
-        y = v.y;
-        z = v.z;
-    }
-};
-
-typedef vec3<int8_t> byteVec3;
-typedef vec3<short> shortVec3;
-typedef vec3<float> floatVec3;
 
 template <typename T>
 class delayBuffer
@@ -59,7 +38,7 @@ public:
 const int sampleRate = 6; // as fast as possible (ends up being every 6 ms)
 const int eventDisplayPeriod = 250; // ms
 
-const float gravityFilterCoeff = 0.01; //0.0001;
+const float gravityFilterCoeff = 0.0001; //0.0001;
 const float gestureThreshold = 0.65;
 const int eventCountThreshold = 3;
 const int minLenThresh = 25; //150*150;
@@ -72,17 +51,18 @@ const int delayBufferSize = dotWavelength2 + meanBufferSize;
 
 
 // Globals
-floatVec3 gravity = {0,0,0};
-floatVec3 currentSample = {0,0,0};
+unsigned long g_prevTime = 0;
+unsigned long g_turnOffDisplayTime = 0;
 
-byteVec3 sampleDelayMem[delayBufferSize];
-delayBuffer<byteVec3> sampleDelay(sampleDelayMem, delayBufferSize);
+floatVec3 g_gravity = {0,0,0};
 
+byteVec3 g_sampleDelayMem[delayBufferSize];
+delayBuffer<byteVec3> g_sampleDelay(g_sampleDelayMem, delayBufferSize);
 
-static float meanDelay1Mem[meanBufferSize];
-float meanDelay2Mem[meanBufferSize];
-delayBuffer<float> meanDelay1(meanDelay1Mem, meanBufferSize);
-delayBuffer<float> meanDelay2(meanDelay2Mem, meanBufferSize);
+float g_meanDelay1Mem[meanBufferSize];
+float g_meanDelay2Mem[meanBufferSize];
+delayBuffer<float> g_meanDelay1(g_meanDelay1Mem, meanBufferSize);
+delayBuffer<float> g_meanDelay2(g_meanDelay2Mem, meanBufferSize);
 
 // Code
 byteVec3 getAccelData()
@@ -99,42 +79,6 @@ void filterVec(const floatVec3& vec, floatVec3& prevVec, float alpha)
     prevVec.z += alpha*(vec.z - prevVec.z);
 }
 
-template<typename T>
-float dot(const vec3<T>& a, const vec3<T>& b)
-{
-    return a.x*b.x + a.y*b.y + a.z*b.z;
-}
-
-/*
-float dotNorm(const floatVec3& a, const floatVec3& b, float minLenThresh)
-{
-    float val = dot(a,b);
-    float aLenSq = dot(a,a);
-    float bLenSq = dot(b,b);
-
-    if (aLenSq < minLenThresh || bLenSq < minLenThresh)
-    {
-        return 0;
-    }
-
-    return val / (sqrt(aLenSq)*sqrt(bLenSq));
-}
-*/
-template <typename T>
-float dotNorm(const vec3<T>& a, const vec3<T>& b, int minLenThresh)
-{
-    float val = dot(a,b);
-    float aLenSq = dot(a,a);
-    float bLenSq = dot(b,b);
-
-    if (aLenSq < minLenThresh || bLenSq < minLenThresh)
-    {
-        return 0;
-    }
-
-    return val / (sqrt(aLenSq)*sqrt(bLenSq));
-}
-
 template <typename T>
 T sum(const T* buf, int len)
 {
@@ -149,38 +93,42 @@ T sum(const T* buf, int len)
 
 float processSample(byteVec3 sample)
 {
-    filterVec(floatVec3(sample), gravity, gravityFilterCoeff);
-    byteVec3 currentSample = byteVec3 {sample.x-gravity.x, sample.y-gravity.y, sample.z-gravity.z};
-    sampleDelay.addSample(currentSample);
+    const bool useMeanBuffer = true;
 
-    // get mean of dot with past
-    /*
-    float val = 0;
-    for(int index = 0; index < meanBufferSize; index++)
-    {
-        float dot1 = dotNorm(sampleDelay.getDelayedSample(index), sampleDelay.getDelayedSample(index+dotWavelength1), minLenThresh);
-        float dot2 = dotNorm(sampleDelay.getDelayedSample(index), sampleDelay.getDelayedSample(index+dotWavelength2), minLenThresh);
-        val += (dot2-dot1);
-    }
-    val /= meanBufferSize;
-    return val;
-    */
-
-    // now add val to mean buffer
-    float dot1 = dotNorm(currentSample, sampleDelay.getDelayedSample(dotWavelength1), minLenThresh);
-    float dot2 = dotNorm(currentSample, sampleDelay.getDelayedSample(dotWavelength2), minLenThresh);
-    meanDelay1.addSample(dot1);
-    meanDelay2.addSample(dot2);
+    filterVec(floatVec3(sample), g_gravity, gravityFilterCoeff);
+    byteVec3 currentSample = byteVec3 {sample.x-g_gravity.x, sample.y-g_gravity.y, sample.z-g_gravity.z};
+    g_sampleDelay.addSample(currentSample);
     
-    float sum1 = sum(meanDelay1Mem, meanBufferSize);
-    float sum2 = sum(meanDelay2Mem, meanBufferSize);
-    return (sum2-sum1) / meanBufferSize;
-
+    // get mean of dot with past
+    if(!useMeanBuffer)
+    {
+        float val = 0;
+        for(int index = 0; index < meanBufferSize; index++)
+        {
+            float dot1 = dotNorm(g_sampleDelay.getDelayedSample(index), g_sampleDelay.getDelayedSample(index+dotWavelength1), minLenThresh);
+            float dot2 = dotNorm(g_sampleDelay.getDelayedSample(index), g_sampleDelay.getDelayedSample(index+dotWavelength2), minLenThresh);
+            val += (dot2-dot1);
+        }
+        val /= meanBufferSize;
+        return val;
+    }
+    else
+    {
+        // now add val to mean buffer
+        float dot1 = dotNorm(currentSample, g_sampleDelay.getDelayedSample(dotWavelength1), minLenThresh);
+        float dot2 = dotNorm(currentSample, g_sampleDelay.getDelayedSample(dotWavelength2), minLenThresh);
+        g_meanDelay1.addSample(dot1);
+        g_meanDelay2.addSample(dot2);
+        
+        float sum1 = sum(g_meanDelay1Mem, meanBufferSize);
+        float sum2 = sum(g_meanDelay2Mem, meanBufferSize);
+        return (sum2-sum1) / meanBufferSize;
+    }
 }
 
-static int count = 0;
 bool filterDetector(float val, float gestureThreshold, int eventCountThreshold)
 {
+    static int count = 0;
     if(val > gestureThreshold)
     {
         count++;
@@ -196,45 +144,70 @@ bool filterDetector(float val, float gestureThreshold, int eventCountThreshold)
     return false;
 }
 
-void app_main()
-{
-    unsigned long prevTime = 0;
-    unsigned long turnOffDisplayTime = 0;
-    uBit.accelerometer.update();
-    gravity = floatVec3(getAccelData()); // init gravity
+enum MicroBitAccelerometerEvents
+    {
+        MICROBIT_ACCELEROMETER_SHAKE = 100
+    };
 
+
+void onShake(MicroBitEvent evt)
+{
+    uBit.display.print('#');
+    // TODO: set a timer to turn off?
+}
+
+bool detectShakeGesture()
+{
+    uBit.accelerometer.update();
+    byteVec3 sample = getAccelData();
+    float val = processSample(sample);
+    return filterDetector(val, gestureThreshold, eventCountThreshold);
+}
+
+/*
+void printStuff()
+{
+    byteVec3 currentSample = byteVec3 {sample.x-g_gravity.x, sample.y-g_gravity.y, sample.z-g_gravity.z};
+    int len = (int)sqrt(currentSample.x*currentSample.x + currentSample.y*currentSample.y + currentSample.z*currentSample.z);
+    printf("%ld\t--\t%d - \t%d\t%d\t%d\t: %d\r\n", (time-g_prevTime), (int)(val*1000), currentSample.x, currentSample.y, currentSample.z, len);
+}
+*/
+
+void accelerometer_poll()
+{
     while(true)
     {
         unsigned long time = uBit.systemTime();
-        // if enough time has elapsed or the timer rolls over, do something
-        if ((time-prevTime) >= sampleRate || time < prevTime) 
+        // If enough time has elapsed or the timer rolls over, do something
+        if ((time-g_prevTime) >= sampleRate || time < g_prevTime) 
         {
-            if (time > turnOffDisplayTime)
+            if (time > g_turnOffDisplayTime)
             {
                 uBit.display.clear();
             }
 
-            uBit.accelerometer.update();
-            auto sample = getAccelData();
-            float val = processSample(sample);
-            bool gotShakeEvent = filterDetector(val, gestureThreshold, eventCountThreshold);
-            if(gotShakeEvent)
+            bool gotShake = detectShakeGesture();
+            if(gotShake)
             {
-                uBit.display.clear();
-                uBit.display.print('x');
-                turnOffDisplayTime = time + eventDisplayPeriod;
-                printf("#### ");
+                g_turnOffDisplayTime = time + eventDisplayPeriod;
+                // has the side-effect of dispatching the event onto the message bus
+                MicroBitEvent(MICROBIT_ID_ACCELEROMETER, MICROBIT_ACCELEROMETER_SHAKE);
             }
-            else
-            {
-                printf("     ");
-            }
-
-            byteVec3 currentSample = byteVec3 {sample.x-gravity.x, sample.y-gravity.y, sample.z-gravity.z};
-            int len = (int)sqrt(currentSample.x*currentSample.x + currentSample.y*currentSample.y + currentSample.z*currentSample.z);
-            printf("%ld\t--\t%d - \t%d\t%d\t%d\t: %d\r\n", (time-prevTime), (int)(val*1000), currentSample.x, currentSample.y, currentSample.z, len);
-
-            prevTime = time;
+            g_prevTime = time;
         }
+        uBit.sleep(1);
     }
+}
+
+void app_main()
+{
+    // init gravity
+    uBit.accelerometer.update();
+    g_gravity = floatVec3(getAccelData());
+    
+    // create background worker that polls for shake events
+    create_fiber(accelerometer_poll);
+
+    // ... and listen for them
+    uBit.MessageBus.listen(MICROBIT_ID_ACCELEROMETER, MICROBIT_ACCELEROMETER_SHAKE, onShake);
 }
